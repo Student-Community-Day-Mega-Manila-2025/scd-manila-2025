4adb58a915e7c53937fc2dd987d4c6a69f2133e7
Ken Velano <120155609+szy-kenn@users.noreply.github.com>
2025-08-08 12:59:44 +0800

    feat: add gradients in event-info section (#13)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,9 @@
 import Countdown from "@/components/common/countdown";
 import EventInfo from "@/components/sections/event-info";
-import FaqSection from "@/components/sections/faqs";
 import Hero from "@/components/sections/hero";
 import MeetTheTeamSection from "@/components/sections/meet-the-team";
 import ProgramFlow from "@/components/sections/program-flow";
 import Registration from "@/components/sections/registration";
-import Speakers from "@/components/sections/speakers";
-import SponsorsAndPartners from "@/components/sections/sponsors-and-partners";
 import Venue from "@/components/sections/venue";
 
 export default function Home() {
@@ -17,9 +14,9 @@ export default function Home() {
       <EventInfo />
       <Venue />
       <ProgramFlow />
-      <Speakers />
-      <SponsorsAndPartners />
-      <FaqSection />
+      {/* <Speakers /> */}
+      {/* <SponsorsAndPartners /> */}
+      {/* <FaqSection /> */}
       <Registration />
       <MeetTheTeamSection />
     </main>
--- src/components/sections/event-info.tsx
+++ src/components/sections/event-info.tsx
@@ -4,6 +4,10 @@ import React from "react";
 const EventInfo = () => {
   return (
     <SectionPage id="event-info" className="relative h-full w-full">
+      <div className="absolute -top-12 right-[15%] -z-20 h-[200px] w-[200px] rounded-full bg-[#79CEE7] opacity-35 blur-[150px]" />
+      <div className="bg-primary-300 absolute -top-40 -left-12 -z-20 hidden h-[500px] w-[400px] rounded-full opacity-70 blur-[150px] lg:flex" />
+      <div className="absolute -top-[40%] right-[40%] -z-20 hidden h-[450px] w-[200px] rotate-[-57deg] rounded-full bg-[#EBE128] opacity-70 blur-[140px] lg:flex" />
+      <div className="bg-primary-300 absolute right-1/5 -bottom-1/2 -z-20 hidden h-[450px] w-[200px] rotate-[-57deg] rounded-full opacity-90 blur-[100px] lg:flex" />
       <div className="mx-auto flex h-fit w-full max-w-7xl flex-col lg:grid lg:h-screen lg:max-h-[650px] lg:grid-cols-2 lg:grid-rows-1">
         <div className="relative flex w-full items-center justify-center p-2 lg:h-full lg:max-w-[600px] lg:px-4 lg:py-8">
           <p className="text-lg">
